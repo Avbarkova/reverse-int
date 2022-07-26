2556fdc822b5d39aa677dfd2ebca92301bb7b5d0
Avbarkova <avbarkova@gmail.com>
2022-07-26 13:26:25 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function reverse (n) {
-  
+    if (n<0) {
+       n = n*(-1);
+    }
+    let arr = n.toString().split('').map(Number);
+    let result = [];
+    if (arr[arr.length-1] == 0) {
+        for (i=arr.length-2; i>=0; i--) {
+        result.push(arr[i]);
+        }
+    } else for (i=arr.length-1; i>=0; i--) {
+        result.push(arr[i]);
+        }
+    return Number(result.join(''));
 }
